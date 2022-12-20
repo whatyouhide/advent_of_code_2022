e11e09be11f1c71d37d460fe2fa381fbf90640b0
Andrea leopardi <an.leopardi@gmail.com>
2022-12-20 11:45:26 +0100

    Cleanups

--- src/day20.rs
+++ src/day20.rs
@@ -5,36 +5,27 @@ use itertools::Itertools;
 const DECRYPTION_KEY: i64 = 811589153;
 
 #[derive(Clone)]
-struct CircularList {
-    elems: Vec<(i64, usize)>,
-    size: usize,
-}
+struct CircularList(Vec<(i64, usize)>);
 
 impl FromIterator<(i64, usize)> for CircularList {
     fn from_iter<I: IntoIterator<Item = (i64, usize)>>(iter: I) -> Self {
-        let elems: Vec<(i64, usize)> = iter.into_iter().collect();
-        let size = elems.len();
-        Self { elems, size }
+        Self(iter.into_iter().collect())
     }
 }
 
 impl Display for CircularList {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.elems.iter().map(|(n, _)| n).join(", "))
+        write!(f, "{}", self.0.iter().map(|(n, _)| n).join(", "))
     }
 }
 
 impl CircularList {
     fn move_element(&mut self, element: (i64, usize)) {
-        let len = self.size as i64;
+        let len = self.0.len() as i64;
         let (number, target_id) = element;
 
         // First, find the number's current position.
-        let current_index = self
-            .elems
-            .iter()
-            .position(|(_, id)| *id == target_id)
-            .unwrap();
+        let current_index = self.0.iter().position(|(_, id)| *id == target_id).unwrap();
 
         // Compute the new index.
         let mut new_index = (current_index as i64 + number) % (len - 1);
@@ -44,14 +35,14 @@ impl CircularList {
         }
 
         // Remove the element from its current position.
-        let element = self.elems.remove(current_index);
-        self.elems.insert(new_index as usize, element);
+        let element = self.0.remove(current_index);
+        self.0.insert(new_index as usize, element);
     }
 
     fn get_element_from_zero(&self, offset_from_zero: u32) -> i64 {
-        let zero_index = self.elems.iter().position(|(n, _)| *n == 0).unwrap();
-        let offset = (offset_from_zero + zero_index as u32) % self.size as u32;
-        self.elems[offset as usize].0
+        let zero_index = self.0.iter().position(|(n, _)| *n == 0).unwrap();
+        let offset = (offset_from_zero + zero_index as u32) % self.0.len() as u32;
+        self.0[offset as usize].0
     }
 }
 
@@ -66,7 +57,7 @@ pub fn run(input: &str) {
     let mut numbers = original_numbers.clone();
 
     for _ in 1..=10 {
-        for element in original_numbers.elems.clone() {
+        for element in original_numbers.0.clone() {
             numbers.move_element(element);
         }
     }
