3f594efdf223cd464ac47372ec3de8192099ab4e
Andrea leopardi <an.leopardi@gmail.com>
2022-12-21 16:21:48 +0100

    Day 21 part 2

--- src/day21.rs
+++ src/day21.rs
@@ -1,6 +1,10 @@
-use std::{collections::HashMap, str::FromStr};
+use std::{
+    collections::HashMap,
+    fmt::{Display, Formatter},
+    str::FromStr,
+};
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Operation {
     Add,
     Sub,
@@ -19,12 +23,21 @@ impl Operation {
         }
     }
 
-    fn apply(&self, a: u64, b: u64) -> u64 {
+    fn apply(&self, a: isize, b: isize) -> isize {
         match self {
             Self::Add => a + b,
             Self::Sub => a - b,
             Self::Mul => a * b,
             Self::Div => a / b,
+        }
+    }
+
+    fn inverse(&self) -> Self {
+        match self {
+            Self::Add => Self::Sub,
+            Self::Sub => Self::Add,
+            Self::Mul => Self::Div,
+            Self::Div => Self::Mul,
         }
     }
 }
@@ -45,113 +58,176 @@ mod operation_test {
 
 #[derive(Debug, PartialEq, Clone)]
 enum Monkey {
-    YellingMonkey(String, u64),
+    YellingMonkey(String, isize),
     MathMonkey(String, Operation, String, String),
 }
 
-#[derive(Debug, PartialEq)]
-struct Node {
-    monkey: Monkey,
-}
-
-use Monkey::*;
-
-impl FromStr for Node {
+impl FromStr for Monkey {
     type Err = ();
 
     fn from_str(string: &str) -> Result<Self, Self::Err> {
         let (name, rest) = string.split_once(":").unwrap();
         let rest = rest.trim();
 
-        match rest.parse::<u64>() {
-            Ok(number) => Ok(Node {
-                monkey: YellingMonkey(name.to_string(), number),
-            }),
+        match rest.parse::<isize>() {
+            Ok(number) => Ok(Self::YellingMonkey(name.to_string(), number)),
             _ => {
                 let (monkey1, rest) = rest.split_at(4);
                 let (operation, monkey2) = rest.trim().split_once(" ").unwrap();
 
-                Ok(Node {
-                    monkey: MathMonkey(
-                        name.to_string(),
-                        Operation::parse(operation).unwrap(),
-                        monkey1.to_string(),
-                        monkey2.to_string(),
-                    ),
-                })
+                Ok(Self::MathMonkey(
+                    name.to_string(),
+                    Operation::parse(operation).unwrap(),
+                    monkey1.to_string(),
+                    monkey2.to_string(),
+                ))
             }
         }
     }
 }
 
-#[cfg(test)]
-mod node_test {
-    use super::*;
-
-    #[test]
-    fn test_from_str() {
-        assert_eq!(
-            Node::from_str("aaaa: 1").unwrap(),
-            Node {
-                monkey: YellingMonkey("aaaa".to_string(), 1)
-            }
-        );
-        assert_eq!(
-            Node::from_str("cccc: aaaa + bbbb").unwrap(),
-            Node {
-                monkey: MathMonkey(
-                    "cccc".to_string(),
-                    Operation::Add,
-                    "aaaa".to_string(),
-                    "bbbb".to_string()
-                )
-            }
-        );
+impl Monkey {
+    fn name(&self) -> String {
+        match self {
+            Self::YellingMonkey(name, _) => name.to_string(),
+            Self::MathMonkey(name, _, _, _) => name.to_string(),
+        }
     }
 }
 
-pub fn run(input: &str) {
-    let monkeys = input
-        .lines()
-        .map(|line| line.parse::<Node>().unwrap())
-        .collect::<Vec<Node>>();
+#[derive(Debug, PartialEq, Clone)]
+enum ASTNode {
+    Human,
+    Number(isize),
+    Operation(Operation, Box<ASTNode>, Box<ASTNode>),
+}
 
-    let mut computed_monkeys = HashMap::new();
-    let mut loop_index = 0;
-
-    loop {
-        loop_index += 1;
-
-        if computed_monkeys.len() == monkeys.len() {
-            break;
-        } else {
-            println!(
-                "{} uncomputed monkeys left in loop {}",
-                monkeys.len() - computed_monkeys.len(),
-                loop_index
-            );
+impl ASTNode {
+    fn from_monkey(monkey: &str, monkeys: &HashMap<String, Monkey>) -> Self {
+        match &monkeys[monkey] {
+            Monkey::YellingMonkey(name, _) if *name == "humn" => Self::Human,
+            Monkey::YellingMonkey(_, number) => Self::Number(*number),
+            Monkey::MathMonkey(_, operation, left, right) => Self::Operation(
+                operation.clone(),
+                Box::new(Self::from_monkey(monkeys[left].name().as_str(), monkeys)),
+                Box::new(Self::from_monkey(monkeys[right].name().as_str(), monkeys)),
+            ),
         }
+    }
 
-        for node in &monkeys {
-            match node.monkey.clone() {
-                YellingMonkey(name, number) => {
-                    computed_monkeys.insert(name, number);
-                }
-                MathMonkey(name, operation, monkey1, monkey2) => {
-                    match (
-                        computed_monkeys.get(&monkey1),
-                        computed_monkeys.get(&monkey2),
-                    ) {
-                        (Some(number1), Some(number2)) => {
-                            let result = operation.apply(*number1, *number2);
-                            computed_monkeys.insert(name, result);
-                        }
-                        _ => continue,
+    fn simplify(&self) -> Self {
+        match self {
+            Self::Human => Self::Human,
+            Self::Number(number) => Self::Number(*number),
+            Self::Operation(operation, left, right) => {
+                let left = left.simplify();
+                let right = right.simplify();
+
+                match (left, right) {
+                    (Self::Number(left), Self::Number(right)) => {
+                        Self::Number(operation.apply(left, right))
+                    }
+                    (left, right) => {
+                        Self::Operation(operation.clone(), Box::new(left), Box::new(right))
                     }
                 }
             }
         }
     }
 
-    println!("The 'root' monkey yells: {}", computed_monkeys["root"]);
+    // left is always an expression with "humn" in it, right is always a number.
+    fn simplify_equation(left: &Self, right: &Self) -> (Self, Self) {
+        match (left, right) {
+            // We solved it!
+            (Self::Human, Self::Number(_)) => (left.clone(), right.clone()),
+
+            (Self::Number(_), _) => panic!("left is a number, but should be humn"),
+
+            // If there's an operation on the left, we simplify it.
+            (Self::Operation(op, x, y), Self::Number(right_number)) => {
+                // The equation is number • y = right_number, so we can simplify it as
+                // y = right_number ¬ number, where ¬ is the opposite of •.
+                match (x.as_ref(), y.as_ref()) {
+                    (Self::Number(number), _) => {
+                        let (simplified_left, op) = if *op == Operation::Sub {
+                            (
+                                Box::new(Self::Operation(
+                                    Operation::Mul,
+                                    Box::new(Self::Number(-1)),
+                                    y.clone(),
+                                )),
+                                Operation::Add,
+                            )
+                        } else {
+                            (y.clone(), *op)
+                        };
+
+                        let simplified_right =
+                            Self::Number(op.inverse().apply(*right_number, *number));
+
+                        Self::simplify_equation(&simplified_left, &simplified_right)
+                    }
+
+                    // The equation is x • number = right_number, so we can simplify it as
+                    // x = right_number ¬ number, where ¬ is the opposite of •.
+                    (_, Self::Number(number)) => {
+                        let simplified_left = x.clone();
+                        let simplified_right =
+                            Self::Number(op.inverse().apply(*right_number, *number));
+
+                        Self::simplify_equation(&simplified_left, &simplified_right)
+                    }
+
+                    _ => {
+                        panic!("found an operation on the left where neither operand is a number")
+                    }
+                }
+            }
+
+            _ => todo!(),
+        }
+    }
+}
+
+impl Display for ASTNode {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Human => write!(f, "humn"),
+            Self::Number(number) => write!(f, "{}", number),
+            Self::Operation(operation, left, right) => write!(
+                f,
+                "({} {} {})",
+                *left,
+                match operation {
+                    Operation::Add => "+",
+                    Operation::Sub => "-",
+                    Operation::Mul => "*",
+                    Operation::Div => "/",
+                },
+                *right
+            ),
+        }
+    }
+}
+
+pub fn run(input: &str) {
+    let monkeys = input
+        .lines()
+        .map(|line| line.parse::<Monkey>().unwrap())
+        .map(|monkey| (monkey.name(), monkey.clone()))
+        .collect::<HashMap<String, Monkey>>();
+
+    let (left_ast, right_ast) = match &monkeys["root"] {
+        Monkey::MathMonkey(_, _, left, right) => (
+            ASTNode::from_monkey(&left, &monkeys),
+            ASTNode::from_monkey(&right, &monkeys),
+        ),
+        _ => panic!("root is not a math monkey"),
+    };
+
+    let simplified = (left_ast.simplify(), right_ast.simplify());
+    println!("Simplified: {} = {}", simplified.0, simplified.1);
+
+    let reduced = ASTNode::simplify_equation(&simplified.0, &simplified.1);
+    println!("Reduced: {} = {}", reduced.0, reduced.1);
 }
